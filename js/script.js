358a91029d32fd0e0510f2f3fe4067456ae6b7e6
Alexander-Eryomenko <sanyochek1991@gmail.com>
2021-01-20 19:47:53 +0200

    first section and burger-menu

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const humburger = document.querySelector('.hamburger'),
+      menu = document.querySelector('.menu'),
+      closeElem = document.querySelector('.menu__close');
+humburger.addEventListener('click', () => {
+  menu.classList.add('active')
+});
+closeElem.addEventListener('click', () => {
+  menu.classList.remove('active')
+});
